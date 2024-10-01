b75dfd608568cda92a609ae35467ad9ac89d79f9
andremc331 <andremc331@gmail.com>
2024-10-01 19:45:48 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ChangeEvent } from "react";
+import Input from "./components/Input";
+
+function App() {
+  return (
+    <div>
+      {/* <Input label={""} value={0} onChange={function (e: ChangeEvent<HTMLInputElement>): void {
+        throw new Error("Function not implemented.");
+      } }>
+        </Input> */}
+    </div>
+  );
+}
+
+export default App;
